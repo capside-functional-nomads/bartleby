af8bdeab234deac938d10c41d3496f0716f0b3e4
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-03-25 18:38:01 +0100

    Create todo table and initial data

---
+++ resources/migrations/20170325182503-sample-data.up.sql
@@ -0,0 +1,5 @@
+insert into todo (description) values ('Schedule a visit with the doctor');
+--;;
+insert into todo (description) values ('Repair TV');
+--;;
+insert into todo (description) values ('Book a table at the new restaurant');
