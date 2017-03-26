b4ad7c3f7a6b0dc7033423256b9e7bd8312872ee
Pau Cervera <pau.cervera@gmail.com>
2017-03-26 10:16:45 +0200

    Serve resources in public

--- src/bartleby/handler.clj
+++ src/bartleby/handler.clj
@@ -1,7 +1,11 @@
-(ns bartleby.handler)
+(ns bartleby.handler
+  (:require [ring.middleware.resource :refer [wrap-resource]]))
 
-(defn app
+(defn ok-app
   [req]
   {:status 200
    :headers {"Content-type" "text/plain"}
    :body "ok"})
+
+(def app (-> ok-app
+             (wrap-resource "public")))
