cc451ed3abc25f57d03238bfb08408e285e3861e
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-04-01 23:47:58 +0200

    Add query to complete task

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -3,3 +3,6 @@ select id, description, done from todo
 
 -- :name find-incomplete-tasks :? *
 select id, description, done from todo where not done
+
+-- :name complete-task :! :n
+update todo set done=true where id=:v:id
